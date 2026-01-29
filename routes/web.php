01028fb57798c950592f821533becdf05978cce0
luigi-raynel-dev <devluigiraynel@gmail.com>
2026-01-29 08:43:55 -0300

    fix: change home page from welcome route to dashboard route if  user is logger else the user will be redirected to login page

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,7 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
-
-Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('home');
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
