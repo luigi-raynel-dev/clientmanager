d61e213a30428efd332839ab7158003103b0af00
luigi-raynel-dev <devluigiraynel@gmail.com>
2026-03-06 09:37:49 -0300

    feat: create migration to create services table

---
+++ database/migrations/2026_03_06_122418_create_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->decimal('base_price', 10, 2)->nullable();
+            $table->enum('price_type', ['fixed', 'unit', 'hourly', 'daily'])->nullable();
+            $table->string('other_price_type', 255)->nullable();
+            $table->decimal('estimated_duration_hours', 8, 2)->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
