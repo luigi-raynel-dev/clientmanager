89bfdbe41c3331caed7ad16d8c9f8acff22de0dc
luigi-raynel-dev <devluigiraynel@gmail.com>
2026-04-02 09:18:16 -0300

    feat: create project_clients table

---
+++ database/migrations/2026_04_02_114149_create_project_clients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_clients', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('client_id');
+            $table->tinyInteger('rating')->nullable();
+            $table->text('feedback')->nullable();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_clients');
+    }
+};
