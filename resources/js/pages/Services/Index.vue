144c8b696d48118ebf4538ff38caa3062dad1e4c
luigi-raynel-dev <devluigiraynel@gmail.com>
2026-04-27 08:58:31 -0300

    feat: refactor services display to use ServiceCard component

--- resources/js/pages/Services/Index.vue
+++ resources/js/pages/Services/Index.vue
@@ -1,20 +1,16 @@
 <script setup lang="ts">
 import { Head, router } from '@inertiajs/vue3';
 import AppLayout from '@/layouts/AppLayout.vue';
-import { create, destroy, edit, index, status } from '@/routes/services';
+import { create, index } from '@/routes/services';
 import { DataPaginator, type BreadcrumbItem } from '@/types';
-import { Card, Paginator } from 'primevue';
+import { Paginator } from 'primevue';
 import debounce from 'lodash.debounce'
 import { ref, watch } from 'vue';
 import ListPageHeading from '@/components/ListPageHeading.vue';
 import SearchField from '@/components/ui/input/SearchField.vue';
 import { Service, ServiceFiltersType } from '@/types/service';
-import { Clock } from 'lucide-vue-next';
 import ServicesFilter from '@/components/Service/ServicesFilter.vue';
-import Actions from '@/components/ui/table/Actions.vue';
-import ToggleStatus from '@/components/ui/toggle/ToggleStatus.vue';
-import TextLimiter from '@/components/TextLimiter.vue';
-import { convertFromMinutes } from '@/utils/time';
+import ServiceCard from '@/components/Service/ServiceCard.vue';
 
 export type ServiceFilterProps = {
   q?: string
@@ -97,42 +93,8 @@ const onPageChange = (event: any) => {
         <p v-else class="text-neutral-500">
           There are {{ props.services.total }} services.
         </p>
-        <Card v-for="service in props.services.data" :key="service.id" class="w-full">
-          <template #title>
-            {{ service.name }}
-          </template>
-          <template #subtitle>
-            <div class="flex w-full gap-6">
-              <span>
-                $ {{ service.base_price || "---" }} / {{
-                  service.pricing_type?.name || 'fixed'
-                }}
-              </span>
-              <span v-if="service.estimated_duration_minutes" class="flex items-center gap-1">
-                <Clock class="size-4" /> {{ convertFromMinutes(service.estimated_duration_minutes,
-                  service.estimated_duration_type) }} {{ service.estimated_duration_type }}
-              </span>
-            </div>
-
-          </template>
-          <template #content v-if="service.description">
-            <TextLimiter :text="service.description" :maxLength="500" />
-          </template>
-          <template #footer>
-            <div class="flex items-center justify-between">
-              <ToggleStatus variant="switch" :status="Boolean(service.is_active)" :url="status(service.id).url"
-                :message="`Do you want to ${service.is_active ? 'deactivate' : 'activate '} this service: #${service.id}?`"
-                :header="service.is_active ? 'Deactivate Service' : 'Activate Service'"
-                :payload="{ is_active: !service.is_active }"
-                :successMessage="`Service ${service.is_active ? 'deactivated' : 'activated'} successfully`"
-                errorMessage="Failed to change service status"
-                :tooltip="service.is_active ? 'Deactivate Service' : 'Activate Service'" />
-              <Actions :edit="{ url: edit(service.id).url }"
-                :delete="{ url: destroy(service.id).url, recordId: service.id }" />
-            </div>
-          </template>
-        </Card>
-
+        <ServiceCard v-for="service in props.services.data" :key="service.id" :service="service" show-action
+          show-description />
         <Paginator :rows="props.services.per_page" :totalRecords="props.services.total" :first="first"
           @page="onPageChange" :rowsPerPageOptions="[5, 10, 25, 50]" />
       </div>
